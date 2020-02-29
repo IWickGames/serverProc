4517c896506f6dbeebbb16b355b8fce48bfa850a
IWickGames <smarttecgaming@gmail.com>
2020-02-29 15:12:44 -0600

    Added TCP

--- serverProc.py
+++ serverProc.py
@@ -2,7 +2,7 @@ import os
 from socket import *
 
 #Client
-def client(ipAddress, portSocket, strMessage):
+def UDPClient(ipAddress, portSocket, strMessage):
     #Encode a string then send it in a packet
     addr = (ipAddress, portSocket)
     UDPSock = socket(AF_INET, SOCK_DGRAM)
@@ -10,7 +10,7 @@ def client(ipAddress, portSocket, strMessage):
     UDPSock.close()
 
 #Server
-def server(ipAddress, portSocket, buffer=1024):
+def UDPServer(ipAddress, portSocket, buffer=1024):
     #Wait untill a packet is resieved and then decode it and output it
     try:
         decode = ''
@@ -25,3 +25,21 @@ def server(ipAddress, portSocket, buffer=1024):
         pass
 
     return decode;
+
+def TCPClient(ipAddress, portSocket, strMessage, buffer=1024):   
+    client = socket()
+    client.connect((ipAddress, portSocket))   
+    client.send(strMessage.encode("utf-8"))
+    data = client.recv(1024).decode("utf-8")
+    client.close()
+    return data;
+
+def TCPServer(ipAddress, portSocket, strMessage,buffer=1024):
+    server = socket(socket.AF_INET, socket.SOCK_STREAM) #Create server socket
+    server.bind((ipAddress, portSocket)) #Bind to IP and Port
+    server.listen(1) #Wait for a connection
+    conn, addr = server.accept() #Accept connection and store IP address
+    data = conn.recv(buffer) #Get there message
+    conn.send(data) #Echo the message back to the client to ensure it was resieved successfully
+    conn.close() #Close socket
+    return addr, data; #Return the address that connected and the data inside
