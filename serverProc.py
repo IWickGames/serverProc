ac62f1cd2b50e1467bcd4b271d186fc12bff159a
IWickGames <smarttecgaming@gmail.com>
2020-03-12 20:44:00 -0500

    Update serverProc.py

--- serverProc.py
+++ serverProc.py
@@ -27,19 +27,21 @@ def UDPServer(ipAddress, portSocket, buffer=1024):
     return str(addr[0]), str(addr[1]), decode; # Sender-IP, Sender-PORT, Decode
 
 def TCPClient(ipAddress, portSocket, strMessage, buffer=1024):   
-    client = socket()
-    client.connect((ipAddress, portSocket))   
-    client.send(strMessage.encode("utf-8"))
-    data = client.recv(1024).decode("utf-8")
-    client.close()
-    return data;
+    client = socket() #Create Socket
+    client.connect((ipAddress, portSocket)) #Connect to the Socket
+    client.send(strMessage.encode("utf-8")) #Send packet
+    data = client.recv(1024).decode("utf-8") #Capture responce packet
+    client.close() #Close client
+    return data; #Return data
 
-def TCPServer(ipAddress, portSocket, strMessage,buffer=1024):
-    server = socket(socket.AF_INET, socket.SOCK_STREAM) #Create server socket
+def TCPServerRecieve(ipAddress, portSocket, strMessage,buffer=1024):
+    server = socket(AF_INET, SOCK_STREAM) #Create server socket
     server.bind((ipAddress, portSocket)) #Bind to IP and Port
     server.listen(1) #Wait for a connection
     conn, addr = server.accept() #Accept connection and store IP address
-    data = conn.recv(buffer) #Get there message
-    conn.send(data) #Echo the message back to the client to ensure it was resieved successfully
+    data = conn.recv(buffer).decode("utf-8") #Get there message
+    return conn, addr, data;
+
+def TCPServerRespond(conn, strMessage):
+    conn.send(strMessage.encode("utf-8")) #Echo the message back to the client to ensure it was resieved successfully
     conn.close() #Close socket
-    return addr, data; #Return the address that connected and the data inside
