32ce79f4b7b24b9e9ef8ee6db09f7a3520b81448
IWickGames <smarttecgaming@gmail.com>
2020-03-12 20:56:13 -0500

    Update ExampleServer.py

--- Examples/TCP/Server/ExampleServer.py
+++ Examples/TCP/Server/ExampleServer.py
@@ -4,4 +4,8 @@ IP = '127.0.0.1'
 PORT = 1111
 BUFFER = 1024
 
-serverProc.TCPServerRecieve(IP, PORT, BUFFER)
+#Wait untill a client sends a request and return it into Message and store there IP in IPAddress
+#connect is the socket save. DO NOT CHANGE OR MESS WITH THAT VARIABLE
+connect, IPAddress, Message = serverProc.TCPServerRecieve(IP, PORT, BUFFER)
+#This will import the connect variable again and return strMessage to the client
+serverProc.TCPServerRespond(connect, "Test Responce")
